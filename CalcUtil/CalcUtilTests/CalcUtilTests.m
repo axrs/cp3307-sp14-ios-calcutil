354ce5b093cde79115cef062d84474cec54ece39
Alexander Scott <xander@cadcoder.com>
2014-04-29 10:24:52 +1000

    XCode Init

---
+++ CalcUtil/CalcUtilTests/CalcUtilTests.m
@@ -0,0 +1,34 @@
+//
+//  CalcUtilTests.m
+//  CalcUtilTests
+//
+//  Created by Alexander Scott on 29/04/2014.
+//  Copyright (c) 2014 CADCoder. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CalcUtilTests : XCTestCase
+
+@end
+
+@implementation CalcUtilTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
